7e1eb9711868a0524a0fda5b97f3ad05fed97124
gwh-red <69139815+gwh-red@users.noreply.github.com>
2026-02-13 09:13:50 +0800

    Add files via upload

---
+++ SpringAiProject/springai_hello/src/main/java/com/gwh/SpringAiDomeApplication.java
@@ -0,0 +1,12 @@
+package com.gwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiDomeApplication.class, args);
+    }
+}
